b31ee7564d610d490829a275f9f0ed3bc3f4b842
YOUR NAMEELI <ELIPEER98@GMAIL.COM>
2016-05-03 23:50:22 -0400

    added another NSLog.

--- Omer/ViewController.m
+++ Omer/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     NSLog(@"people");
+    NSLog(@"cute");
+    //people
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
