0ca4c535d3fe35d35672f3797bc46f4cd731d77d
YOUR NAMEELI <ELIPEER98@GMAIL.COM>
2016-05-03 22:41:23 -0400

    added an slog for experiment

--- Omer/ViewController.m
+++ Omer/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"people");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
